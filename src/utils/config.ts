bae88adc1d738fcadede5ea11c6e8ee04fdd178c
onpine <718215627@qq.com>
2023-04-13 22:28:08 +0800

    chore: build

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const baseUrls = {
   development: "http://zinchon.com:334",
-  production: "http://127.0.0.1",
+  production: "http://zinchon.com:334",
 };
 
 export const BASE_URL = baseUrls[process.env.NODE_ENV];
